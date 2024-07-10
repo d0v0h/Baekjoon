e7180110d84b816feaa6d3d6c1ae4be7527dc51a
d0v0h <139430360+d0v0h@users.noreply.github.com>
2024-07-10 09:55:34 +0900

    [Bronze II] Title: Hashing, Time: 36 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -6,8 +6,12 @@ l = int(input())
 alpha = {chr(i): i-96 for i in range(97,123)}
 string = input().strip()
 
+power_31 = [1]
+for i in range(1, l):
+    power_31.append(power_31[i-1] * 31)
+
 result = 0
 for i, char in enumerate(string):
-    result += alpha[char] * 31 ** i
+    result += alpha[char] * power_31[i]
 
-print(result)
+print(result % 1234567891)
