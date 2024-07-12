22afcbdbf200718af458f966e9c3a15f0e883658
d0v0h <139430360+d0v0h@users.noreply.github.com>
2024-07-12 21:39:33 +0900

    [Silver I] Title: IOIOI, Time: 356 ms, Memory: 34048 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -2,18 +2,24 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-s = int(input())
-m = input().strip()
+m = int(input())
+s = input().strip()
 
-pn = 'IOI'
-
-for i in range(1, n):
-    pn += 'OI'
-
+pn = 'IO' * n + 'I'
 length = len(pn)
-count = 0
-for i in range(s-length+1):
-    if m[i:i+length] == pn:
-        count += 1
 
-print(count)
+i = cnt = ans = 0
+
+while i < m-1:
+    if s[i:i+3] == 'IOI':   # i ~ i+2까지 'IOI'이면
+        i += 2              # 2칸 건너 뜀
+        cnt += 1            # IOI 하나 발생 체크
+        if cnt == n:        # IOI 발생과 pn이 일치하면
+            ans += 1        # 정답 카운트 1 증가
+            cnt -= 1        # 이동한 슬라이딩 윈도우 왼쪽부분 제거
+    
+    else:
+        i += 1              # IOI가 아니면 한칸 미룸
+        cnt = 0             # 연속 IOI가 깨졌으므로 0으로 초기화
+
+print(ans)
