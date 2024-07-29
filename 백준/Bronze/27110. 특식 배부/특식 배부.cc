d3ef1fb114793ea99a8a3f4458d480f40d09401a
d0v0h <139430360+d0v0h@users.noreply.github.com>
2024-07-29 17:15:17 +0900

    [Bronze IV] Title: 특식 배부, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/27110. 특식 배부/특식 배부.cc
+++ 백준/Bronze/27110. 특식 배부/특식 배부.cc
@@ -1,28 +1,12 @@
 #include <iostream>
 using namespace std;
 
-int calc(int person, int n) {
-    if (person > n) {
-        return n;
-    }
-    else {
-        return person;
-    }
-}
-
 int main(){
     int n, A, B, C;
     cin >> n;
 
     cin >> A >> B >> C;
 
-    int result = 0;
-
-    result += calc(A, n);
-    result += calc(B, n);
-    result += calc(C, n);
-
+    int result = ( A > n ? n : A) + ( B > n ? n : B) + ( C > n ? n : C);
     cout << result << endl;
-
-    return 0;
 }
