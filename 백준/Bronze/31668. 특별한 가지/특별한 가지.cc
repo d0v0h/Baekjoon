eedaa0e032d6216924372e2f2ac75ba16a8b4cc0
Donghee KIM <139430360+d0v0h@users.noreply.github.com>
2024-08-09 16:55:08 +0900

    [Bronze IV] Title: 특별한 가지, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31668. 특별한 가지/특별한 가지.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m, k;
+    cin >> n;
+    cin >> m;
+    cin >> k;
+
+    int result = m / n * k;
+    cout << result << endl;
+    return 0;
+}
