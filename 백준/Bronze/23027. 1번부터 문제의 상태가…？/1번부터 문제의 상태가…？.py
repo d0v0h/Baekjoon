4ed8b984504b47a22bff4904d3cd9d61491f8a95
Donghee KIM <139430360+d0v0h@users.noreply.github.com>
2024-08-13 11:07:51 +0900

    [Bronze II] Title: 1번부터 문제의 상태가…?, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/23027. 1번부터 문제의 상태가…？/1번부터 문제의 상태가…？.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+s = input().strip()
+
+if 'A' in s:
+    s = s.replace('B', 'A')
+    s = s.replace('C', 'A')
+    s = s.replace('D', 'A')
+    s = s.replace('F', 'A')
+
+elif 'B' in s:
+    s = s.replace('C', 'B')
+    s = s.replace('D', 'B')
+    s = s.replace('F', 'B')
+
+elif 'C' in s:
+    s = s.replace('D', 'C')
+    s = s.replace('F', 'C')
+
+else:
+    s = 'A' * len(s)
+
+print(s)
