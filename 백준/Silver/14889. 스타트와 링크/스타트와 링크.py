9241f1fb8ba67d191028eb17cf83622f6efd4f50
Donghee KIM <139430360+d0v0h@users.noreply.github.com>
2024-09-03 14:14:59 +0900

    [Silver I] Title: 스타트와 링크, Time: 4992 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -1,27 +1,33 @@
-from itertools import combinations, permutations
 import sys; input = sys.stdin.readline
 
 N = int(input())
+
 graph = []
-comb_arr = [i for i in range(N)]
 for _ in range(N):
     graph.append(list(map(int, input().split())))
+visited = [False for _ in range(N)]
 
-MIN = 1e9
-for start in combinations(comb_arr, N//2):
-    s, l = 0, 0
-    link = []
-    for i in range(N):
-        if i not in start:
-            link.append(i)
+result = 0
+MIN = int(1e9)
+def btk(n, start):
+    global MIN
+    if n == N//2:
+        start, link = 0, 0,
 
-
-    for perm in permutations(start, 2):
-        s += graph[perm[0]][perm[1]]
+        for i in range(N):
+            for j in range(N):
+                if not visited[i] and not visited[j]:
+                    link += graph[i][j]
+                if visited[i] and visited[j]:
+                    start += graph[i][j]
     
-    for perm in permutations(link, 2):
-        l += graph[perm[0]][perm[1]]
+        MIN = min(MIN, abs(start - link))
+        return
 
-    MIN = min(MIN, abs(s - l))
+    for i in range(start, N):
+        visited[i] = True
+        btk(n+1, i+1)
+        visited[i] = False
 
+btk(0, 0)
 print(MIN)
