c67215507c421793babe560d096fb9ac5aeafb3c
Donghee KIM <139430360+d0v0h@users.noreply.github.com>
2024-09-21 21:39:03 +0900

    [Bronze III] Title: 지능형 기차, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2455. 지능형 기차/지능형 기차.py
+++ 백준/Bronze/2455. 지능형 기차/지능형 기차.py
@@ -1,17 +1,9 @@
 import sys; input = sys.stdin.readline
 
-arr = [0 for _ in range(4)]
+arr = [0 for _ in range(5)]
 
-for idx in range(4):
+for idx in range(1, 5):
     o, i = map(int, input().split())
-    
-    if idx == 0:
-        arr[idx] += i
-    
-    elif idx == 3:
-        arr[idx] += o
-    
-    else:
-        arr[idx] = arr[idx-1] + (i - o)
+    arr[idx] = arr[idx-1] + (i - o)
 
 print(max(arr))
