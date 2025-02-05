81bbd4e6807cbd29d8c91a3923249336feeaaee4
Donghee Kim <139430360+d0v0h@users.noreply.github.com>
2025-02-05 18:52:53 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 560 ms, Memory: 62948 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.py
@@ -0,0 +1,33 @@
+from collections import deque
+import sys; input = sys.stdin.readline
+
+n, m, r = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+visited = [0] * (n+1)
+
+def bfs(node):
+    queue = deque([node])
+    cnt = 1
+    visited[node] = cnt
+
+    while queue:
+        cur_node = queue.popleft()
+        for next_node in graph[cur_node]:
+            if not visited[next_node]:
+                queue.append(next_node)
+                cnt += 1
+                visited[next_node] = cnt
+
+    print(*visited[1:], sep='\n')
+
+# 그래프 입력
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+# 오름 차순 정렬
+for item in graph:
+    item.sort(reverse=True)
+
+bfs(r)
