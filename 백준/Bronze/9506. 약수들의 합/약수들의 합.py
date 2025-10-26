66c8e276edbf3887ab227d60b9500e5f0db7585b
Donghee Kim <139430360+d0v0h@users.noreply.github.com>
2025-10-26 12:26:56 +0900

    [Bronze I] Title: 약수들의 합, Time: 60 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/9506. 약수들의 합/약수들의 합.py
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.py
@@ -1,15 +1,8 @@
-n = int(input())
-
+n=int(input())
 while n != -1:
-    temp = []
-    for i in range(1, n+1):
+    r=[]
+    for i in range(1,n):
         if n % i == 0:
-            temp.append(i)
-    
-    if sum(temp[:-1]) == n:
-        print(n, '= ', end='')
-        print(*temp[:-1], sep=' + ')
-    else:
-        print(n, 'is NOT perfect.')
-    
-    n = int(input())
+            r.append(i)
+    print(f"{n} =", ' + '.join(map(str,r))) if sum(r) == n else print(f"{n} is NOT perfect.")
+    n=int(input())
