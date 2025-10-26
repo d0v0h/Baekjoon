8709ec19c54a2c1fad31dabb21f83cde56cc2a9f
Donghee Kim <139430360+d0v0h@users.noreply.github.com>
2025-10-26 12:49:06 +0900

    [Bronze II] Title: 소수, Time: 604 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2581. 소수/소수.py
+++ 백준/Bronze/2581. 소수/소수.py
@@ -1,17 +1,11 @@
-m = int(input())
-n = int(input())
-
-result = []
-for i in range(m, n+1):
+m=int(input())
+n=int(input())
+r=[]
+for i in range(m,n+1):
     if i > 1:
-        for j in range(2, i):
+        for j in range(2,i):
             if i % j == 0:
                 break
         else:
-            result.append(i)
-
-if result:
-    print(sum(result))
-    print(min(result))
-else:
-    print(-1)
+            r.append(i)
+print(sum(r), min(r), sep='\n') if len(r) != 0 else print(-1)
