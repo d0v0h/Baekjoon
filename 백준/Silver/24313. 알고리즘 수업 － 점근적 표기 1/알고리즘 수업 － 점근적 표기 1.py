9cb9a191cbc8d6ad98bed1045c43562f5c27dd00
Donghee Kim <139430360+d0v0h@users.noreply.github.com>
2025-10-28 11:59:02 +0900

    [Silver V] Title: 알고리즘 수업 - 점근적 표기 1, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.py
+++ 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.py
@@ -1,10 +1,5 @@
-import sys; input = sys.stdin.readline
-
-a1, a0 = map(int, input().split())
-c = int(input())
-n_0 = int(input())
-
-if (a1 * n_0 + a0) <= (c * n_0) and a1 <= c:
+a1,a0,c,n0=map(int,open(0).read().split())
+if c >= a1 and a1*n0+a0 <= c*n0:
     print(1)
 else:
     print(0)
